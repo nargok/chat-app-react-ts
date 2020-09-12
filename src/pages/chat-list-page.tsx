92a966c444748105dd1cf0ff6a8c92f1d93e0f73
nargok <nargok@gmail.com>
2020-09-13 00:05:55 +0900

    button

--- src/pages/chat-list-page.tsx
+++ src/pages/chat-list-page.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Button from "../components/button";
 
 class ChatListPage extends React.Component {
   render() {
-    return <div>チャットルームリスト</div>;
+    return (
+      <div>
+        チャットルームリスト
+        <Button name="登録" onClick={() => console.log("登録")} primary />
+        <Button name="そのほか" onClick={() => console.log("そのほか")} />
+      </div>
+    );
   }
 }
 
