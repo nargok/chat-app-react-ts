391e7c7dc8703404aaa32619a948dfc18040a380
nargok <nargok@gmail.com>
2020-09-12 23:51:12 +0900

    global css

---
+++ src/styles/global.ts
@@ -0,0 +1,17 @@
+import { css } from "@emotion/core";
+
+const globals = css`
+  html,
+  body {
+    font-size: 15px;
+    width: 100%;
+    height: 100%;
+    font-family: sans-serif;
+    overscroll-behavior: none;
+    padding: 0;
+    margin: 0;
+    overflow: hidden;
+  }
+`;
+
+export default globals;
